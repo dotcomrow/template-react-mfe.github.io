b98b3021f369abc478885774e6b0b46ee652f1b2
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-16 21:20:28 -0400

    ?
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/utilities/constants.js
+++
@@ -1,7 +0,0 @@
-export const lookupCodesCacheKey = "lookupCodes";
-
-export const lookupCodes = Object.freeze({
-    SESSION_TTL_SECONDS: 'SESSION_TTL_SECONDS',
-    GOOGLE_CLIENT_ID: 'GOOGLE_CLIENT_ID',
-    
-});
