63255c4a8b503e2afa363c855fa1060c18c9828b
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-16 21:19:07 -0400

    initial
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ src/components/component.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import "../utilities/constants";
+
+const object = ({
+  vars
+}) => {
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div className="bg-white p-dynamic h-100">
+      <div className="container h-100"></div>
+    </div>
+  );
+};
+
+export default object;
